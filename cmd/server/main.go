ef78e86cbc9bb292598fcdaa56dde3e05e9f5985
the-1aw <bguilliod@gmail.com>
2026-01-20 00:08:35 +0100

    feat: connect server to amqp server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const amqpConnectionString = "amqp://guest:guest@localhost:5672/"
+
+func hangUntilInterrupt() {
+	terminationChan := make(chan os.Signal, 1)
+	signal.Notify(terminationChan, os.Interrupt)
+	<-terminationChan
+}
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	amqpConnection, err := amqp.Dial(amqpConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpConnection.Close()
+	fmt.Println("Connection to amqp was successful...")
+	hangUntilInterrupt()
+	fmt.Println("\nShutting down amqp server...")
 }
--- internal/gamelogic/logs.go
+++ internal/gamelogic/logs.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/the-1aw/peril/internal/routing"
 )
 
 const logsFile = "game.log"
