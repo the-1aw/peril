db961ce932e3865751144742cfaa3fdc4a1e682c
the-1aw <bguilliod@gmail.com>
2026-01-21 00:23:00 +0100

    feat: init server gameloop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/the-1aw/peril/internal/gamelogic"
+	"github.com/the-1aw/peril/internal/pubsub"
+	"github.com/the-1aw/peril/internal/routing"
 )
 
 const amqpConnectionString = "amqp://guest:guest@localhost:5672/"
-
-func hangUntilInterrupt() {
-	terminationChan := make(chan os.Signal, 1)
-	signal.Notify(terminationChan, os.Interrupt)
-	<-terminationChan
-}
 
 func main() {
 	amqpConnection, err := amqp.Dial(amqpConnectionString)
@@ -23,7 +18,28 @@ func main() {
 		log.Fatal(err)
 	}
 	defer amqpConnection.Close()
-	fmt.Println("Connection to amqp was successful...")
-	hangUntilInterrupt()
-	fmt.Println("\nShutting down amqp server...")
+	channel, err := amqpConnection.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	gamelogic.PrintServerHelp()
+	for {
+		userInputs := gamelogic.GetInput()
+		if len(userInputs) == 0 {
+			continue
+		}
+		switch userInputs[0] {
+		case "pause":
+			fmt.Println("Pausing the game")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+		case "resume":
+			fmt.Println("Resuming the game")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+		case "quit":
+			fmt.Println("Quitting the game")
+			return
+		default:
+			fmt.Println("Unknown command: ", userInputs[0])
+		}
+	}
 }
